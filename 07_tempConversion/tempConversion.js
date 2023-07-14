eeb51f0f761830d394fdc6de985f03d2f1a6817d
koekjehebbe <mrnmaarten@gmail.com>
2023-07-14 17:47:03 +0200

    finished exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(givenTemp) {
+  convertedTemp = (givenTemp - 32)*(5/9);
+  const roundedTemp = convertedTemp.toFixed(1);
+  console.log (roundedTemp);
+  return Number(roundedTemp);
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(givenTemp) {
+  convertedTemp = (givenTemp * (9/5) + 32);
+  const roundedTemp = convertedTemp.toFixed(1);
+  console.log (roundedTemp);
+  return Number(roundedTemp);
 };
 
 // Do not edit below this line
