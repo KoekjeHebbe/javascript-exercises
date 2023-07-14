6990357e1d9dca300f9ed0b13aa51f42c9d6967b
koekjehebbe <mrnmaarten@gmail.com>
2023-07-14 17:33:29 +0200

    finished exercise 06, I liked it a lot, conditionals are my thang

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
-
+const leapYears = function(theYear) {
+    if ((theYear % 4 === 0) || ((theYear % 400 === 0) && (theYear % 100 !== 0))){
+       if (theYear % 100 === 0){
+        if (theYear % 400 === 0){
+            return true
+        }
+        else {
+            return false
+        }
+       }
+       else {
+        return true
+       }
+    }
+    else{
+        return false
+    }
 };
 
 // Do not edit below this line
