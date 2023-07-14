177f6ac284e0cef981fb47ff9050c8726436538b
koekjehebbe <mrnmaarten@gmail.com>
2023-07-14 11:37:06 +0200

    finished exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, inputNum) {
+    if (inputNum < 0){
+        return "ERROR"
+    }
+    else {
+    let repeatedString = inputString.repeat(inputNum);
+    return repeatedString;
+    }
 };
 
 // Do not edit below this line
