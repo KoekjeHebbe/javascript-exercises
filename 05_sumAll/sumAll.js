87981babeba4baacd52509829501bc7e7699044f
koekjehebbe <mrnmaarten@gmail.com>
2023-07-14 17:11:48 +0200

    finished exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(startNum,endNum) {
+   if (startNum>0 && endNum>0 && (typeof endNum === "number") && (typeof startNum === "number")){ 
+    if (startNum<endNum){
+    sum = (endNum * (endNum + startNum))/2
+    console.log(sum);
+    return (sum);
+    }
+    else if (startNum>endNum){
+    sum = (startNum * (endNum + startNum))/2
+    console.log(sum);
+    return (sum);
+    }
+    else{
+        return "ERROR";
+    }
+    }
+    else {
+        return "ERROR";
+    }
+//  const sum = newArray.reduce((a, b) => a + b, 0);
+//  console.log(sum);
 
 };
 
