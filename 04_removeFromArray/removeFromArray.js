162993a547b032ce2348043deefac76f4afa36c6
koekjehebbe <mrnmaarten@gmail.com>
2023-07-14 15:00:43 +0200

    finished exercise 03

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, toRemove) {
+    let removeArray = [arguments[1],arguments[2],arguments[3],arguments[4]];
+    let newArray = inputArray.filter((value) => {
+        return !removeArray.includes(value)
+    })
+    return newArray
+    console.log(newArray)
 };
 
 // Do not edit below this line
