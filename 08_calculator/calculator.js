eb75d80d71ffc2355052d022883b0fa2cb8e3ce2
koekjehebbe <mrnmaarten@gmail.com>
2023-08-04 14:05:25 +0200

    finished exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(c) {
+	let sum = 0;
+  for (let i = 0; i < c.length; i++ ) {
+    sum += c[i];
+  }
+  return sum
 };
 
 const multiply = function() {
-
+  let sum = 1;
+  for (let j = 0; j < arguments.length; j++ ) {
+    sum *= arguments[j];
+  }
+  return sum
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  const factorialOf = num => {
+    let factorial = 1;
+  
+    for(let i = 1; i <= num; i++) {
+      factorial *= i;
+    }
+  
+    return factorial;
+  }
+  return factorialOf(num);
+  
 };
 
 // Do not edit below this line
