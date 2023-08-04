2a3f62ee5c8d69bb90658fa88ccc1d5505aa32a9
koekjehebbe <mrnmaarten@gmail.com>
2023-08-04 15:01:17 +0200

    finished exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+b = 0;
+c = 1;
+a = a -1;
+if (a < 0){
+    return "OOPS"
+} else
+for (let i = 0; i < a; i++){
+    d = b + c;
+    b = c;
+    c = d;
+}
+return c
 };
 
 // Do not edit below this line
