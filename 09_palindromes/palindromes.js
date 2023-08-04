b3abc49d5aadf6eb1a3b3ab65faed31007d121e6
koekjehebbe <mrnmaarten@gmail.com>
2023-08-04 14:47:52 +0200

    finished exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    const onlyLettersNumbers = a.toLowerCase().replace(/[^a-z0-9]/g, "");
+    const word = onlyLettersNumbers.split('');
+    const reversed = word.reverse('');
+    const reversedString = reversed.join('');
+    if (onlyLettersNumbers === reversedString){
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
