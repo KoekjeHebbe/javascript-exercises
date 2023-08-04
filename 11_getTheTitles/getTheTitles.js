3bd824f701f1dcd08e98a5fc193b79d0f9907cf6
koekjehebbe <mrnmaarten@gmail.com>
2023-08-04 15:10:52 +0200

    finished exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(original) {
+    const newArray = original.map(({title, author})=> (title));
+    return newArray
 };
 
 // Do not edit below this line
